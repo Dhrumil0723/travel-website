4005d310748a35474541cb101798bf8e40d659db
Vinit <vs361017@gmail.com>
2021-07-19 10:13:11 +0530

    added events, culture, and stories section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "./Components/Header/header";
 import TopPart from "./Components/Top-section/TopPart";
+import RecPlaces from "./Components/Rec-places/RecPlaces";
+import CulturePage from "./Components/Culture-page/CulturePage";
+import StoriesSection from "./Components/Stories-section/StoriesSection";
 
 import "./App.css";
 // import VisitorForm from "./Components/VisitorForm";
@@ -11,13 +14,15 @@ function App() {
       <div
         style={{
           background: `url(./assets/image.png) no-repeat center center/cover`,
-          height: "100vh",
+          height: "110vh",
         }}
       >
         <Header />
         <TopPart />
+        <RecPlaces />
+        <CulturePage />
+        <StoriesSection />
       </div>
-      {/* <VisitorForm /> */}
     </div>
   );
 }
